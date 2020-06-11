ad969d24c48f6b78cbc63b438970f1b402d26b1f
veedee <43088920+veedee2000@users.noreply.github.com>
2020-06-11 13:53:10 +0530

    Create Trailing_Zeroes_Factorial.cpp

---
+++ Day3/Trailing_Zeroes_Factorial.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int base = 5;
+    while(n / base){
+        ans += n / base;
+        base *= 5;
+    }
+    
+    cout << ans;
+    
+	return 0;
+}
