a6c04e0dee0221efb89469dc744422bd5f6ac1db
veedee <43088920+veedee2000@users.noreply.github.com>
2020-06-28 21:39:23 +0530

    Create BFS.cpp

---
+++ Day17/Right_Side_View/BFS.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int>ans;
+        queue<TreeNode*>q;
+        if(root) q.push(root);
+        
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i = 0;i < sz;i++){
+                TreeNode* curr = q.front();
+                if(i == 0) ans.push_back(curr -> val);
+                if(curr -> right) q.push(curr -> right);
+                if(curr -> left) q.push(curr -> left);
+                q.pop();
+            }
+        }
+        
+        return ans;
+    }
+};
