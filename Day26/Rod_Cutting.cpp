bbbb9994af376abc1400408209d0f6af2d18ae18
veedee <43088920+veedee2000@users.noreply.github.com>
2020-07-14 19:24:18 +0530

    Update Rod_Cutting.cpp

--- Day26/Rod_Cutting.cpp
+++ Day26/Rod_Cutting.cpp
@@ -1,29 +1,32 @@
-
-int f(int n, vector<int>& nums, vector<int>& dp){
-    if(n == 0) return 0;
-    if(n < 0) return INT_MIN;
-    if(dp[n] != INT_MIN) return dp[n];
-    for(auto num:nums){
-        int val = f(n - num, nums, dp);
-        if(val == INT_MIN) continue;
-        dp[n] = max(dp[n], 1 + val);
+int f(int beg, int end, vector<int>& nums, vector<vector<unsigned long long>>& dp, vector<vector<int>>& path){
+    if(beg + 1 == end) return 0;
+    if(dp[beg][end] != LLONG_MAX) return dp[beg][end];
+    int len = nums[end] - nums[beg], val, index;
+    
+    for(int k = beg + 1;k < end;k++){
+        val = len + f(beg, k, nums, dp, path) + f(k, end, nums, dp, path);
+        if(dp[beg][end] > val) dp[beg][end] = val, index = k;
     }
-    return dp[n];
+    path[beg][end] = index;
+    return dp[beg][end];
 }
 
-int main() {
-    
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        vector<int>nums(3, 0);
-        for(int i = 0;i < 3;i++) cin >> nums[i];
-        vector<int>dp(n + 1, INT_MIN);
-        f(n, nums, dp);
-        cout << (dp[n] == INT_MIN ? -1 : dp[n]) << endl;
-    }
-    
-	return 0;
+void pathCal(int i, int j, vector<int>& sol, vector<int>& nums, vector<vector<int>>& path){
+    if(i + 1 == j) return;
+    sol.push_back(nums[path[i][j]]);
+    pathCal(i, path[i][j], sol, nums, path);
+    pathCal(path[i][j], j, sol, nums, path);
+}
+
+vector<int> Solution::rodCut(int A, vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    nums.insert(nums.begin(), 0);
+    nums.push_back(A);
+    int n = nums.size();
+    vector<vector<unsigned long long>>dp(n, vector<unsigned long long>(n, LLONG_MAX));
+    vector<vector<int>>path(n, vector<int>(n));
+    f(0, n - 1, nums, dp, path);
+    vector<int>sol;
+    pathCal(0, n - 1, sol, nums, path);
+    return sol;
 }
